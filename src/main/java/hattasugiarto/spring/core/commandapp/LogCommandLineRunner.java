7f1f49f42caecf97539215ac8ae9d98108239332
Hatta56 <hattasugiarto443@gmail.com>
2023-02-03 08:38:13 +0700

    Command Line Runner

---
+++ src/main/java/hattasugiarto/spring/core/commandapp/LogCommandLineRunner.java
@@ -0,0 +1,17 @@
+package hattasugiarto.spring.core.commandapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+@Slf4j
+@Component
+public class LogCommandLineRunner implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Log Command Line Runner : {}", Arrays.toString(args));
+    }
+}
